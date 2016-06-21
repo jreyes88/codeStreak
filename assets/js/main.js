e1c36d7c24ac609b9aad524ee3a7a26c31c480ea
Joey Reyes <jreyes88@gmail.com>
2016-06-20 20:43:49 -0500

    running stuff into firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
-var startTime;
-var endTime;
+$(document).ready(function() {
+	var timeData = new Firebase("https://codetracker.firebaseio.com/");
+	var startTime;
+	var endTime;
 
-$("#startButton").on("click", function() {
-	startTime = moment(startTime).format("MM/DD/YY hh:mm:ss");
-	console.log(startTime);
-});
+	$("#startButton").on("click", function() {
+		startTime = moment(startTime).format("MM/DD/YY hh:mm:ss");
+		console.log(startTime);
 
-$("#endButton").on("click", function() {
-	endTime = moment(endTime).format("MM/DD/YY hh:mm:ss");
-	console.log(endTime);
+		$("#endButton").on("click", function() {
+			endTime = moment(endTime).format("MM/DD/YY hh:mm:ss");
+			endDate = moment(endTime).format("MM/DD/YY");
+			console.log(endTime);
+
+			var newTimeInput = {
+				codeDate: endDate,
+				minutes: diffTime
+			}
+
+			var diffTime = moment(endTime).diff(moment(startTime), "minutes");
+			console.log(diffTime);
+			timeData.push(newTimeInput);
+		});
+		return false;
+	});
 });
