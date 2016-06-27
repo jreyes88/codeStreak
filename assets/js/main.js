9710fac1f884e9cd705ad8b6e4d3737f4745b498
Joey Reyes <jreyes88@gmail.com>
2016-06-26 22:42:14 -0500

    i have started building the chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,25 +52,49 @@ $(document).ready(function() {
 		var diffTime = childSnapshot.val().minutes;
 		var projectInputName = childSnapshot.val().projectName;
 
-		// // First Time (pushed back 1 year to make sure it comes before current time)
-		// var firstTimeConverted = moment(firstTrainTime,"hh:mm").subtract(1, "years");
-
-		// // Prettify the train start
-		// var currentTime = moment();
-
-		// // Difference between current time and train's first time
-		// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-		// // Time apart (remainder)
-		// var tRemainder = diffTime % trainFrequency;
-
-		// // Minutes until train
-		// var tMinutesTillTrain = trainFrequency - tRemainder;
-
-		// // Next Train
-		// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-
 		// Add each train's data into the table
 		$("#projectTable > tbody").append("<tr><td>" + startTime + "</td><td>" + endTime + "</td><td>" + diffTime + "</td><td>" + projectInputName + "</td></tr>");
 	});
+
+	// context for chart
+	var ctx = $("#myChart");
+	var myLineChart = new Chart(ctx, {
+	    type: 'line',
+	    data: data,
+	    options: {
+	        scales: {
+	            xAxes: [{
+	                display: false
+	            }]
+	        }
+	    }
+	});
+
+	var data = {
+	    labels: ["January", "February", "March", "April", "May", "June", "July"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            fill: false,
+	            lineTension: 0.1,
+	            backgroundColor: "rgba(75,192,192,0.4)",
+	            borderColor: "rgba(75,192,192,1)",
+	            borderCapStyle: 'butt',
+	            borderDash: [],
+	            borderDashOffset: 0.0,
+	            borderJoinStyle: 'miter',
+	            pointBorderColor: "rgba(75,192,192,1)",
+	            pointBackgroundColor: "#fff",
+	            pointBorderWidth: 1,
+	            pointHoverRadius: 5,
+	            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+	            pointHoverBorderColor: "rgba(220,220,220,1)",
+	            pointHoverBorderWidth: 2,
+	            pointRadius: 1,
+	            pointHitRadius: 10,
+	            data: [65, 59, 80, 81, 56, 55, 40],
+	        }
+	    ]
+	};
+
 });
